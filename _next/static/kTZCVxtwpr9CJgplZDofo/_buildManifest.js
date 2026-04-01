c5ee31d43ead017795fde3824b513d8f6e4739c9
angel667 <hellsgame667@gmail.com>
2026-04-01 14:24:22 +0500

    Updates

---
+++ _next/static/kTZCVxtwpr9CJgplZDofo/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/home_nursing_candidate_portal//_next/:path+",
+        "destination": "/home_nursing_candidate_portal/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
